9982abb9e9797b6a6506c55d649c326e8fdad650
seungahpyun <spyun@student.codam.nl>
2025-05-07 10:15:04 +0200

    clone mlx42

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/07 09:59:02 by spyun         #+#    #+#                 */
-/*   Updated: 2025/05/07 09:59:57 by spyun         ########   odam.nl         */
+/*   Updated: 2025/05/07 10:13:29 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,47 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <math.h>
-# include "libft/libft.h"
+# include "libft.h"
+# include "MLX42.h"
+
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
+
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	move_speed;
+	double	rot_speed;
+}	t_player;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	char		*no_path;
+	char		*so_path;
+	char		*we_path;
+	char		*ea_path;
+	t_color		floor;
+	t_color		ceiling;
+	char		**map;
+	int			map_width;
+	int			map_height;
+	int			player_x;
+	int			player_y;
+	char		player_dir;
+	t_player	player;
+	// t_texture	texture[4];
+	// t_img		img;
+}	t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,21 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/07 09:52:59 by spyun         #+#    #+#                 */
-/*   Updated: 2025/05/07 09:56:29 by spyun         ########   odam.nl         */
+/*   Updated: 2025/05/07 10:11:26 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "cub3d.h"
 
-int main (int argc, char **argv)
+int	main (int argc, char **argv)
 {
+	t_game	game;
+
+	if (argc != 2)
+	{
+		write(2, "Error\nUsage: ./cub3d <map.cub>\n", 32);
+		return (1);
+	}
+
 
 }
