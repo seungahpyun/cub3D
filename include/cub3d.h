92ec5e77acea68bd4985a92d58dc767e895ae006
seungahpyun <spyun@student.codam.nl>
2025-05-07 10:01:08 +0200

    basic file added

---
+++ include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cub3d.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <spyun@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/07 09:59:02 by spyun         #+#    #+#                 */
+/*   Updated: 2025/05/07 09:59:57 by spyun         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <math.h>
+# include "libft/libft.h"
+
+#endif
