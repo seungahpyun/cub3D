c845e7ed4be55da4e27a911b31a7bd49dac1a5be
jsong <jsong@student.codam.nl>
2025-05-12 18:04:11 +0200

    added draw_walls

--- src/render/minimap.c
+++ src/render/minimap.c
@@ -6,7 +6,7 @@
 /*   By: jsong <jsong@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/09 15:21:43 by jsong         #+#    #+#                 */
-/*   Updated: 2025/05/12 17:57:38 by jsong         ########   odam.nl         */
+/*   Updated: 2025/05/12 18:02:09 by jsong         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,21 +43,44 @@ static int	calculate_cell_size(t_game *game)
 	game->map_height = 6;
 	cell_h = (float)MINIMAP_H / game->map_height;
 	cell_size = (int)fminf(cell_w, cell_h);
+	if (cell_size < 1)
+		cell_size = 1;
 	return (cell_size);
+}
+
+static void	draw_walls(t_game *game)
+{
+	int	mx;
+	int	my;
+	int	cell_size;
+
+	mx = 0;
+	my = 0;
+	game->map_height = 6;
+	cell_size = calculate_cell_size(game);
+	while (mx < game->map_width)
+	{
+		my = 0;
+		while (my < game->map_height)
+		{
+			if (game->map[my][mx] == '1')
+				draw_cell(game->minimap, mx * cell_size, my * cell_size,
+					cell_size, MINIMAP_WALL_COLOR);
+			my++;
+		}
+		mx++;
+	}
 }
 
 void	render_minimap(t_game *game)
 {
-	int	cell_size;
-
 	game->minimap = mlx_new_image(game->mlx, MINIMAP_W, MINIMAP_H);
 	if (!game->minimap)
 		ft_mlx_error(game);
 	// Set every pixel to white
 	ft_memset(game->minimap->pixels, 255, game->minimap->width
 		* game->minimap->height * sizeof(int32_t));
-	cell_size = calculate_cell_size(game);
-	draw_cell(game->minimap, 0, 0, cell_size, MINIMAP_WALL_COLOR);
+	draw_walls(game);
 	if (mlx_image_to_window(game->mlx, game->minimap, 0, 0) < 0)
 		ft_mlx_error(game);
 }
