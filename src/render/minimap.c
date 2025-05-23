ace32b180ccf7adb90faa1f862323cf0f5ddc9c2
seungahpyun <spyun@student.codam.nl>
2025-05-23 17:11:50 +0200

    without offset working much better

--- src/render/minimap.c
+++ src/render/minimap.c
@@ -6,7 +6,7 @@
 /*   By: jsong <jsong@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/09 15:21:43 by jsong         #+#    #+#                 */
-/*   Updated: 2025/05/23 15:23:41 by spyun         ########   odam.nl         */
+/*   Updated: 2025/05/23 17:10:51 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,13 @@ static void	draw_minimap_cell(t_game *game, int i, int j)
 	int					mx;
 	int					my;
 	t_cell				cell;
-	t_minimap_offset	offset;
 
-	offset.offset_x = game->player.x - (int)game->player.x;
-	offset.offset_y = game->player.y - (int)game->player.y;
 	mx = (int)floor(game->player.x) - MINIMAP_RADIUS + i;
 	my = (int)floor(game->player.y) - MINIMAP_RADIUS + j;
 	if (is_valid_point(&game->map, mx, my))
 	{
-		cell.px = i * MINIMAP_CELL_SIZE \
-			- (int)(offset.offset_x * MINIMAP_CELL_SIZE);
-		cell.py = j * MINIMAP_CELL_SIZE \
-			- (int)(offset.offset_y * MINIMAP_CELL_SIZE);
+		cell.px = i * MINIMAP_CELL_SIZE;
+		cell.py = j * MINIMAP_CELL_SIZE;
 		cell.size = MINIMAP_CELL_SIZE;
 		cell.color = get_color(game->map.grid[my][mx]);
 		draw_cell(game->minimap.img, cell);
@@ -62,6 +57,35 @@ static void	draw_minimap_grid(t_game *game)
 	}
 }
 
+static void	calculate_player_screen_pos(t_game *game, t_point *pos)
+{
+	double	player_screen_x;
+	double	player_screen_y;
+
+	player_screen_x = (game->player.x
+			- ((int)game->player.x - MINIMAP_RADIUS)) * MINIMAP_CELL_SIZE;
+	player_screen_y = (game->player.y
+			- ((int)game->player.y - MINIMAP_RADIUS)) * MINIMAP_CELL_SIZE;
+	pos->x = (int)player_screen_x;
+	pos->y = (int)player_screen_y;
+}
+
+static void	draw_single_ray(t_game *game, t_point start, int i)
+{
+	t_point	end;
+	double	adjusted_distance;
+
+	adjusted_distance = game->rays[i].dist * 0.98;
+	end.x = start.x + cos(game->rays[i].angle)
+		* adjusted_distance * MINIMAP_CELL_SIZE;
+	end.y = start.y - sin(game->rays[i].angle)
+		* adjusted_distance * MINIMAP_CELL_SIZE;
+	if (end.x >= 0 && end.x < MINIMAP_W && end.y >= 0 && end.y < MINIMAP_H
+		&& start.x >= 0 && start.x < MINIMAP_W
+		&& start.y >= 0 && start.y < MINIMAP_H)
+		draw_line(game->minimap.img, start, end, MINIMAP_RAY_COLOR);
+}
+
 /**
  * Draws rays on minimap from the player:
  * 1. Calculate the ray angle and distiance to get hit point of ray
@@ -70,38 +94,26 @@ static void	draw_minimap_grid(t_game *game)
  */
 static void	draw_minimap_rays(t_game *game)
 {
-	t_minimap_offset	offset;
-	t_point				start;
-	t_point				end;
-	int					i;
+	t_point	start;
+	int		i;
 
-	offset.offset_x = game->player.x - (int)game->player.x;
-	offset.offset_y = game->player.y - (int)game->player.y;
-	start.x = MINIMAP_W / 2 - (int)(offset.offset_x * MINIMAP_CELL_SIZE);
-	start.y = MINIMAP_H / 2 - (int)(offset.offset_y * MINIMAP_CELL_SIZE);
+	calculate_player_screen_pos(game, &start);
 	i = 0;
 	while (i < WIDTH)
 	{
-		end.x = start.x + cos(game->rays[i].angle) \
-			* game->rays[i].dist * MINIMAP_CELL_SIZE;
-		end.y = start.y - sin(game->rays[i].angle) \
-			* game->rays[i].dist * MINIMAP_CELL_SIZE;
-		draw_line(game->minimap.img, start, end, MINIMAP_RAY_COLOR);
+		draw_single_ray(game, start, i);
 		i++;
 	}
 }
 
 static void	draw_minimap_player(t_game *game)
 {
-	t_cell				player_cell;
-	t_minimap_offset	offset;
+	t_cell	player_cell;
+	t_point	pos;
 
-	offset.offset_x = game->player.x - (int)game->player.x;
-	offset.offset_y = game->player.y - (int)game->player.y;
-	player_cell.px = MINIMAP_W / 2 - (MINIMAP_CELL_SIZE * 3 / 4) / 2
-		- (int)(offset.offset_x * MINIMAP_CELL_SIZE);
-	player_cell.py = MINIMAP_H / 2 - (MINIMAP_CELL_SIZE * 3 / 4) / 2
-		- (int)(offset.offset_y * MINIMAP_CELL_SIZE);
+	calculate_player_screen_pos(game, &pos);
+	player_cell.px = pos.x - (MINIMAP_CELL_SIZE * 3 / 4) / 2;
+	player_cell.py = pos.y - (MINIMAP_CELL_SIZE * 3 / 4) / 2;
 	player_cell.size = MINIMAP_CELL_SIZE * 3 / 4;
 	player_cell.color = MINIMAP_PLAYER_COLOR;
 	draw_cell(game->minimap.img, player_cell);
